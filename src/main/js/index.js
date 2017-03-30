94eaf8668c3721e74d7758be48ace814b7243f26
Ole Ersoy <ole.ersoy@gmail.com>
2017-03-30 00:33:26 -0500

    Usable release

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -4,45 +4,45 @@
 //==================================================
 //  Tasks
 //  ---------------------------------------------------
-//  deploy
-//  ---------------------------------------------------
-//  Deploys both main and test css and test html
 //  ___________________________________________________
 //  ---------------------------------------------------
-//  deploy:css
+//  deploy:main:css
 //  ---------------------------------------------------
-//  Deploys main and test css only
+//  Deploys main css only
 //  ___________________________________________________
 //  ---------------------------------------------------
-//  deploy:html
+//  deploy:test:css
 //  ---------------------------------------------------
-//  Deploys main and test css only
+//  Deploys test css only
+//  ___________________________________________________
+//  ---------------------------------------------------
+//  deploy:main:html
+//  ---------------------------------------------------
+//  Deploys main html only
+//  ___________________________________________________
+//  ---------------------------------------------------
+//  Deploys test html only
 //  ___________________________________________________
 //  ---------------------------------------------------
 //  IMPLEMENTATION
 //  ---------------------------------------------------
-//  deploy:css
+//  deploy:main:css and deploy:test:css
 //  ---------------------------------------------------
-//  This reruns the all the plugins in configured in
-//  superfly-css-task-build.  The reason is that font magician
-//  has to be run after uncss is run.  That way only fonts
-//  that are used have @font-face declarations generated.
-//  Main and test css is sourced and the result of the task
-//  is output to the corresponding deploy directories.
+//  These tasks rerun all the plugins in configured in
+//  superfly-css-task-build task.  The reason is that
+//  font magician  has to be run after uncss is run.
+//  That way only fonts  that are used will have a
+//  corresponding @font-face declaration generated.
 //  ---------------------------------------------------
-//  deploy:html
+//  deploy:main:html and deploy:test:html
 //  ---------------------------------------------------
-//  This updates the css import of the corresponding test html
-//  files.  Only the reference to the `index.css` file is updated.
-//  In other words:
+//  Deploying html files updates the css import directory to the `deploy`
+//  directory.  In other words if the css import is currently being done like
+//  this:
 //  <link rel="stylesheet" type="text/css" href="../../../target/test/css/index.css">
-//  turns into:
+//  After deployment it will be done like this:
 //  <link rel="stylesheet" type="text/css" href="../../../deploy/test/css/index.css">
 //  ---------------------------------------------------
-//
-//  1: Cheerio copies content from `Test-markup`
-//     and appends it to the markup to the `Test-render`
-//
 //==================================================
 
 var gulp = require('gulp');
@@ -71,6 +71,20 @@ var post_uncss_processors = [pc_font_magician, autoprefixer, pc_reporter({
   clearMessages: true
 })];
 
+gulp.task('deploy:main:css', function() {
+  return gulp.src(PLI.SRC_MAIN_CSS)
+    .pipe(pc(pre_uncss_processors))
+    .pipe(uncss({
+      html: [PLI.SRC_MAIN_HTML]
+    }))
+    .pipe(pc(post_uncss_processors))
+    .pipe(cleancss({
+      compatibility: 'ie8'
+    }))
+    .pipe(gulp.dest(PLI.deploy.main.css));
+});
+
+
 gulp.task('deploy:test:css', function() {
   return gulp.src(PLI.SRC_TEST_CSS)
     .pipe(pc(pre_uncss_processors))
@@ -84,6 +98,16 @@ gulp.task('deploy:test:css', function() {
     .pipe(gulp.dest(PLI.deploy.test.css));
 });
 
+gulp.task('deploy:main:html', function() {
+  return gulp
+    .src(PLI.TARGET_MAIN_HTML)
+    .pipe(cheerio(function($, file) {
+      $('link[href="../../../target/test/css/index.css"]').attr('href',
+        $('link[href="../../../target/test/css/index.css"]').attr('href').replace('target', 'deploy'));
+    }))
+    .pipe(gulp.dest(PLI.deploy.main.html));
+});
+
 gulp.task('deploy:test:html', function() {
   return gulp
     .src(PLI.TARGET_TEST_HTML)
@@ -93,43 +117,3 @@ gulp.task('deploy:test:html', function() {
     }))
     .pipe(gulp.dest(PLI.deploy.test.html));
 });
-
-/*
-
-gulp.task('deploy:css', function() {
-  gulp
-    .src(PLI.SRC_MAIN_HTML)
-    .pipe(gulp.dest(PLI.deploy.main.html));
-
-  return gulp
-    .src(PLI.SRC_TEST_HTML)
-    .pipe(gulp.dest(PLI.deploy.test.html));
-});
-
-
-gulp.task('deploy:main', function() {
-
-  gulp.src(PLI.TARGET_MAIN_CSS)
-    .pipe(uncss({
-      html: [PLI.TARGET_MAIN_HTML]
-    }))
-    .pipe(gulp.dest(PLI.deploy.main.css));
-
-  return gulp
-    .src(PLI.TARGET_MAIN_HTML)
-    .pipe(gulp.dest(PLI.deploy.main.html));
-});
-
-gulp.task('deploy:test', function() {
-
-  gulp.src(PLI.TARGET_TEST_CSS)
-    .pipe(uncss({
-      html: [PLI.TARGET_TEST_HTML]
-    }))
-    .pipe(gulp.dest(PLI.deploy.test.css));
-
-  return gulp
-    .src(PLI.TARGET_TEST_HTML)
-    .pipe(gulp.dest(PLI.deploy.test.html));
-});
-*/
